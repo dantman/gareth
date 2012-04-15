872a3eed087a2d6065ad50669568ac131a2248e6
Daniel Friesen <pub-github@nadir-seen-fire.com>
2012-04-14 18:48:26 -0700

    Initial commit of a bare Django project for the 2nd Gareth rewrite.

---
+++ gareth/settings_user-sample.py
@@ -0,0 +1,18 @@
+
+# Enable development settings
+from settings_dev import *
+
+# Setup the database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = [...]
