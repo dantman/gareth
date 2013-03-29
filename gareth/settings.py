0d3645d785a4607c62eb355463aae9db6f2d1452
Daniel Friesen <daniel@nadir-seen-fire.com>
2013-03-29 11:56:48 -0700

    Set STOMP to None in the main settings so that we don't get an import error when someone forgets to define it in their settings.

--- gareth/settings.py
+++ gareth/settings.py
@@ -6,6 +6,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+STOMP = None
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
